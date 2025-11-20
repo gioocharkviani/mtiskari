ca5b83df4a3215ec97df94d42638d60216e6a5c0
gioocharkviani <gioocharkviani@gmail.com>
2025-11-20 23:08:31 +0400

    fix/bg=position

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Header from "../layout/Header";
 const Hero = () => {
   return (
     <div className="w-full flex justify-center min-h-screen relative">
-      <div className="w-full h-full absolute hero-section bg-right! mb:bg-center! z-1"></div>
+      <div className="w-full h-full absolute hero-bg bg-position-[calc(100%+0px)_center]! bg-cover!  md:bg-center! z-1"></div>
       <div className="hero-bg-overlay absolute z-2"></div>
       <div className="absolute w-full z-3">
         <Header />
